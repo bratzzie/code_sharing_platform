6f11126a546e904fc4af3dbd6519f15686bd8b6b
Anastasiia Bichurina <bichurina.anastasiia@gmail.com>
2022-03-11 18:57:35 +0200

    snippets feed

--- src/main/java/com/stud/codesharing/ApplicationController.java
+++ src/main/java/com/stud/codesharing/ApplicationController.java
@@ -2,48 +2,84 @@ package com.stud.codesharing;
 
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
-import org.springframework.web.servlet.ModelAndView;
 
-import java.util.HashMap;
-import java.util.Map;
+import java.util.*;
+import java.util.stream.Collectors;
 
-@RestController
+@org.springframework.stereotype.Controller
 public class ApplicationController {
-    Code currentCodeSnippet = new Code("", "");
+    List<Code> list = new ArrayList<>();
 
-    @GetMapping(value = "/code")
-    public ModelAndView getAsHtml(Model model) {
-        model.addAttribute("code_obj", currentCodeSnippet);
-
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("result");
-        return modelAndView;
-    }
-
-    @GetMapping("/api/code")
+    @GetMapping("/api/code/{N}")
     @ResponseBody
-    public Map<String, String> getAsJson(){
+    public Map<String, String> getAsJson(@PathVariable int N){
         Map<String, String> responseMap = new HashMap<>();
-        responseMap.put("code", currentCodeSnippet.getCode());
-        responseMap.put("date", currentCodeSnippet.getDate());
-
+        if (N < list.size()) {
+            Code currentCodeSnippet = list.get(N);
+            if (currentCodeSnippet != null) {
+                responseMap.put("code", currentCodeSnippet.getCode());
+                responseMap.put("date", currentCodeSnippet.getDate());
+                return responseMap;
+            }
+        }
+        responseMap.put("code", "");
+        responseMap.put("date", "");
         return responseMap;
     }
 
+    @GetMapping(value = "/code/{N}")
+    public String getAsHtml(@PathVariable int N, Model model) {
+        if (N < list.size()) {
+            Code currentCodeSnippet = list.get(N);
+            if (currentCodeSnippet != null) {
+                model.addAttribute("code_obj", currentCodeSnippet);
+                return "result";
+            }
+        }
+
+        Code nullCode = new Code("", "");
+        model.addAttribute("code_obj", nullCode);
+        return "result";
+    }
+
     @GetMapping("/code/new")
+    public String createNewCodeInHtml() {
+        return "index";
+    }
+
+    @GetMapping("/api/code/latest")
     @ResponseBody
-    public ModelAndView createNewCodeInHtml() {
-        ModelAndView modelAndView = new ModelAndView();
-        modelAndView.setViewName("index");
-        return modelAndView;
+    public List<Code> showLatestAsJson(){
+        return getLatestSnippets();
+    }
+
+    @GetMapping("/code/latest")
+    public String showLatestInHtml(Model model) {
+        List<Code> currentList = getLatestSnippets();
+        model.addAttribute("latestSnippets", currentList);
+        return "latestResult";
     }
 
     @PostMapping("/api/code/new")
     @ResponseBody
     public Map<String, String> postNewCode(@RequestBody Code code) {
-        currentCodeSnippet = code;
-        currentCodeSnippet.setDate();
-        return new HashMap<>();
+        code.setNotFormattedDate();
+        code.setDate();
+
+        list.add(code);
+        Map<String, String> resultToReturn = new HashMap<>();
+        resultToReturn.put("id", String.valueOf(list.indexOf(code)));
+        return resultToReturn;
+    }
+
+    private List<Code> getLatestSnippets() {
+        List<Code> currentList = new ArrayList<>(list);
+
+        Comparator<Code> comparator = Comparator.comparing(Code::getNotFormattedDate);
+        currentList.sort(comparator);
+        Collections.reverse(currentList);
+
+        return currentList.stream().limit(10).collect(Collectors.toList());
     }
 
 }
--- src/main/java/com/stud/codesharing/Code.java
+++ src/main/java/com/stud/codesharing/Code.java
@@ -1,18 +1,22 @@
 package com.stud.codesharing;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+
 
 public class Code {
     private String code;
     private String date;
     private final String DATE_FORMATTER= "yyyy-MM-dd HH:mm:ss";
+    private LocalDateTime notFormattedDate;
 
 
     public Code(String codeSnippet, String timeInFormat) {
         code = codeSnippet;
         date = timeInFormat;
+        this.notFormattedDate = LocalDateTime.now();
     }
 
     public Code() {
@@ -35,8 +39,17 @@ public class Code {
     }
 
     public void setDate() {
-        LocalDateTime uploadedTime = LocalDateTime.now();
+        LocalDateTime uploadedTime = getNotFormattedDate();
         DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_FORMATTER);
         date = uploadedTime.format(formatter);
+    }
+
+    @JsonIgnore
+    public LocalDateTime getNotFormattedDate() {
+        return notFormattedDate;
+    }
+
+    public void setNotFormattedDate() {
+        this.notFormattedDate = LocalDateTime.now();
     }
 }
